6c874cabd25b4dddc7b27c248b369305cbaa4051
host4ideas <felixmb99@hotmail.es>
2022-02-21 19:56:58 +0100

    updated composer, implementing mail verification

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -11,42 +11,83 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
+
+// class RegistrationController extends AbstractController
+// {
+//     /**
+//      * @Route("/register", name="app_register")
+//      */
+//     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, UsersAuthenticator $authenticator): Response
+//     {
+//         $user = new Users();
+//         $form = $this->createForm(RegistrationFormType::class, $user);
+//         $form->handleRequest($request);
+
+//         if ($form->isSubmitted() && $form->isValid()) {
+//             // encode the plain password
+//             $user->setPassword(
+//                 $passwordEncoder->encodePassword(
+//                     $user,
+//                     $form->get('plainPassword')->getData()
+//                 )
+//             );
+
+//             $entityManager = $this->getDoctrine()->getManager();
+//             $entityManager->persist($user);
+//             $entityManager->flush();
+//             // do anything else you need here, like send an email
+
+//             return $guardHandler->authenticateUserAndHandleSuccess(
+//                 $user,
+//                 $request,
+//                 $authenticator,
+//                 'main' // firewall name in security.yaml
+//             );
+//         }
+
+//         return $this->render('registration/register.html.twig', [
+//             'registrationForm' => $form->createView(),
+//         ]);
+//     }
+// }
 
 class RegistrationController extends AbstractController
 {
-    /**
-     * @Route("/register", name="app_register")
-     */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, UsersAuthenticator $authenticator): Response
+    private $verifyEmailHelper;
+    private $mailer;
+
+    public function __construct(VerifyEmailHelperInterface $helper, MailerInterface $mailer)
     {
-        $user = new Users();
-        $form = $this->createForm(RegistrationFormType::class, $user);
-        $form->handleRequest($request);
+        $this->verifyEmailHelper = $helper;
+        $this->mailer = $mailer;
+    }
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-                $passwordEncoder->encodePassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
+    /**
+     * @Route("/register", name="register-user")
+     */
+    public function register(): Response
+    {
+        $user = new User();
 
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
-            // do anything else you need here, like send an email
+        // handle the user registration form and persist the new user...
 
-            return $guardHandler->authenticateUserAndHandleSuccess(
-                $user,
-                $request,
-                $authenticator,
-                'main' // firewall name in security.yaml
-            );
-        }
+        $signatureComponents = $this->verifyEmailHelper->generateSignature(
+            'registration_confirmation_route',
+            $user->getId(),
+            $user->getEmail()
+        );
 
-        return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form->createView(),
-        ]);
+        $email = new TemplatedEmail();
+        $email->from('send@example.com');
+        $email->to($user->getEmail());
+        $email->htmlTemplate('registration/confirmation_email.html.twig');
+        $email->context(['signedUrl' => $signatureComponents->getSignedUrl()]);
+
+        $this->mailer->send($email);
+
+        // generate and return a response for the browser
     }
 }
